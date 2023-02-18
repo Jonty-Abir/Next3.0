09a374ed6ba2a4cef15106db95417ee24bfd2771
Abir Santra <90158767+Jonty-Abir@users.noreply.github.com>
2023-02-18 18:28:15 +0530

    3.0

--- instance/instance.ts
+++ instance/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
-  // baseURL: "https://authentication-with-next-auth.vercel.app",
+ // baseURL: "http://localhost:3000",
+   baseURL: "https://authentication-with-next-auth.vercel.app",
 });
 
 export { instance };
